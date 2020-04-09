bb10aedc02679f1b1f5447c85bfbfbb0c3993d78
KristjanTreimann <43129577+KristjanTreimann@users.noreply.github.com>
2020-04-09 19:24:30 +0300

    Added header component
    #4

---
+++ components/Navbar.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+nav.navbar.navbar-default.light-shadow.container-fluid.hidden-xs(id="main-menu")
+    .text-center
+        ul.nav.navbar-nav(role="navigation")
+            li.dropdown
+                a.dropdown-toggle(aria-expanded="false" data-toggle="dropdown" v-for="(link, i) in importantLinks" :key="i" href="link.href") {{link.text}}
+</template>
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+        importantLinks: [
+            {
+            text: 'IGAPÄEVAPANGANDUS',
+            href: 'https://vuetifyjs.com',
+            },
+            {
+            text: 'RAHA KASVATAMINE',
+            href: 'https://community.vuetifyjs.com',
+            },
+            {
+            text: 'LAEN JA LIISING',
+            href: 'https://madewithvuejs.com/vuetify',
+            },
+            {
+            text: 'PENSION',
+            href: 'https://twitter.com/vuetifyjs',
+            },
+            {
+            text: 'VAJAD ABI?',
+            href: 'https://medium.com/vuetify',
+            }
+      ]
+        }
+    },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,27 @@
 <template lang="pug">
 .container
-	div
-		Header
-		br
-		br
-		h1.title lhv2.0
-		h2.subtitle Kasutajaliideste disain ja arendus
-		.links
-			a.button--green(href="https://nuxtjs.org/" target="_blank") Documentation
-			a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank") GitHub
-		Footer
+	Header
+	Navbar
+	br
+	br
+	h1.title lhv2.0
+	h2.subtitle Kasutajaliideste disain ja arendus
+	.links
+		a.button--green(href="https://nuxtjs.org/" target="_blank") Documentation
+		a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank") GitHub
+	Footer
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue"
-import Footer from "~/components/Footer.vue"
-import Header from "~/components/Header.vue"
+import Footer from '~/components/Footer.vue'
+import Header from '~/components/Header.vue'
+import Navbar from '~/components/Navbar.vue'
 
 export default {
   components: {
-	Logo,
 	Footer,
-	Header
+	Header,
+	Navbar
   }
 }
 </script>
